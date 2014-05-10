491ec0aade258e3f4883c2f9acf93752daa49890
Ryan Van Etten <ryanve@gmail.com>
2014-05-10 15:12:47 -0400

    Ensure that `new energy` works for use like `new EventEmitter`

--- test/index.js
+++ test/index.js
@@ -2,58 +2,65 @@
   var common = typeof module != 'undefined' && !!module.exports
     , aok = common ? require('aok') : root.aok
     , energy = common ? require('../src') : root.energy
-    , emitter = energy()
+    , emitters = [energy(), new energy]
     , key = 'key'
     , indexOf = [].indexOf
-    , ran = false
 
   function noop() {}
   function yes() { return true }
-   
-  aok('instance', emitter instanceof energy)
-  aok('listeners', typeof emitter.listeners(key).length == 'number')
   
-  !function(id) {
-    var bool = true, gone = indexOf ? function(what) {
-      return -1 === emitter.listeners(key).indexOf(what)
-    } : yes
+  function test(emitter, i) {
+    var ran = []
     
-    emitter.on(key, noop).off(key, noop)
-    if (bool = gone(noop)) {
-      emitter.on(key, noop).on(key, noop).off(key, noop)
-      bool = gone(noop)
-    }
+    aok('instance-' + i, emitter instanceof energy)
+    aok('listeners-' + i, typeof emitter.listeners(key).length == 'number')
     
-    aok(id, bool)
-    emitter.off(key, noop)
-  }('off')
+    !function(id) {
+      var bool = true, gone = indexOf ? function(what) {
+        return -1 === emitter.listeners(key).indexOf(what)
+      } : yes
+      
+      emitter.on(key, noop).off(key, noop)
+      if (bool = gone(noop)) {
+        emitter.on(key, noop).on(key, noop).off(key, noop)
+        bool = gone(noop)
+      }
+      
+      aok(id, bool)
+      emitter.off(key, noop)
+    }('off')
 
-  emitter.off(key).once(key, function() {
-    ran = true
-    aok('once', 0 === emitter.listeners(key).length)
-  }).emit(key)
+    emitter.off(key).once(key, function() {
+      ran[i] = true
+      aok('once-' + i, 0 === emitter.listeners(key).length)
+    }).emit(key)
+    
+    aok('to', function() {
+      var bool = wannabe === energy.to(wannabe) && typeof wannabe.emit == 'function'
+      function wannabe() {
+        wannabe.emit('called')
+      }
+
+      bool && wannabe.once('called', function() {
+        bool = wannabe === this
+      }) && wannabe()
+      return bool
+    })
+    
+    !function(id, n) {
+      var i = n, initial = emitter.listeners(key).length
+      emitter.off(key, noop)
+      while (i--) emitter.on(key, noop)
+      aok(id, n === emitter.listeners(key).length - initial)
+      emitter.off(key, noop)
+    }('repeat', 3)
+    
+    setTimeout(function() {
+      if (ran[i]) aok.info('All tests ran for group ' + i)
+      else aok.warn('Group ' + i + ' tests did not complete.')
+    }, 0)
+  }
   
-  aok('to', function() {
-    var bool = wannabe === energy.to(wannabe) && typeof wannabe.emit == 'function'
-    function wannabe() {
-      wannabe.emit('called')
-    }
-
-    bool && wannabe.once('called', function() {
-      bool = wannabe === this
-    }) && wannabe()
-    return bool
-  })
-  
-  !function(id, n) {
-    var i = n, initial = emitter.listeners(key).length
-    emitter.off(key, noop)
-    while (i--) emitter.on(key, noop)
-    aok(id, n === emitter.listeners(key).length - initial)
-    emitter.off(key, noop)
-  }('repeat', 3)
-
-  setTimeout(function() {
-    ran ? aok.info('All tests ran.') : aok.warn('Some tests did not run yet.')
-  }, 0)
+  aok.pass(emitters, test)
+  if (common) aok(require('deep-equal').apply(null, emitters))
 }(this);
